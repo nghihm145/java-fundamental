2c34541ee506a3ebc17a44ffc2ff3c62ed06332a
nghidadev <mannghi25@gmail.com>
2024-12-18 19:14:36 +0700

    update something

--- src/_02VariablesAndTypes/f06FloatingPointVariables.java
+++ src/_02VariablesAndTypes/f06FloatingPointVariables.java
@@ -1,9 +1,10 @@
 package _02VariablesAndTypes;
-/*Viết chương trình Java để khởi tạo 2 kiểu dữ liệu double và float.
-Định nghĩa rằng kiểu dữ liệu thứ hai được khai báo là kiểu float và in các giá trị ra 2 dòng khác nhau
-Đầu vào: không có
-Đầu ra: số kiểu float và số kiểu double, mỗi số trên một dòng
-Thời gian chạy:  1.5 giây*/
+/* Viết chương trình Java để khởi tạo 2 kiểu dữ liệu double và float.
+ * Định nghĩa rằng kiểu dữ liệu thứ hai được khai báo là kiểu float và in các giá trị ra 2 dòng khác nhau
+ * Đầu vào: không có
+ * Đầu ra: số kiểu float và số kiểu double, mỗi số trên một dòng
+ * Thời gian chạy:  1.5 giây
+ */
 public class f06FloatingPointVariables {
     public static void main(String[] args) {
         double myDouble = 5.5;
